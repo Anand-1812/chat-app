1d814b8ba86a65251da4adf748006437709666ac
Anand <anand.work2005@gmail.com>
2025-08-10 11:49:21 +0530

    completed the project

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ chat.on('message', (user, message) => {
 });
 
 
-chat.on('join', (user) => {
-  console.log(`${user} has joined the chat.\n`);
+chat.on('logout', (user) => {
+  console.log(`${user} has left the chat.\n`);
 });
+
+// simulating the chat
+
+chat.join('Alice')
+chat.join('Bob');
+
+chat.sendMessage('Alice', "Hey Alice, hello to everyone.");
+chat.sendMessage('Bob', "Hey bob, hello to everyone.");
+
+chat.leaveChat('Alice\n');
+chat.sendMessage('Alice', "this message won't sent");
+chat.leaveChat('Bob');
+
+
