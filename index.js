f368f259f7c1d99248f31d66ea8939167311eacd
Anand <anand.work2005@gmail.com>
2025-08-10 11:42:43 +0530

    create the functionality

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log('working');
+const ChatRoom = require('./chatRoom.js');
 
+const chat = new ChatRoom();
+
+chat.on('join', (user) => {
+  console.log(`${user} has joined the chat.\n`);
+});
+
+
+chat.on('message', (user, message) => {
+  console.log(`${user} : ${message}.\n`);
+});
+
+
+chat.on('join', (user) => {
+  console.log(`${user} has joined the chat.\n`);
+});
